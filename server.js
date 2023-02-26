cc3e1bf198ec08141739b2aee0e5d0da4b07d482
DangB1910209 <dangB1910209@student.ctu.edu.vn>
2023-02-26 14:56:04 +0700

    Cai dat cac handler truy xuat CSDL

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const app = require('./app')
 const config = require('./app/config')
+const MongoDB = require('./app/utils/mongodb.util')
 
-const PORT = config.app.port;
+async function startServer() {
+    try {
+        await MongoDB.connect(config.db.uri)
+        console.log(`Connected to database with uri: ${config.db.uri}`);
+        const PORT = config.app.port;
 
-app.listen(PORT, () => {
-    console.log('Server is running !')
-})
+        app.listen(PORT, () => {
+            console.log(`Server is running on PORT: ${PORT}!`)
+        })
+    } catch (error) {
+        console.log(error)
+        process.exit()
+    }
+}
+
+startServer()
